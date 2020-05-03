c6554507da2e51b3dced59553516cc14c8d3513c
mike-baumgarten <32228925+mike-baumgarten@users.noreply.github.com>
2020-05-02 23:04:28 -0600

    Created a button called Sing and made it sing when clicked

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-var friends = ['Matt', 'Joe', 'Danica', 'MeiLee', 'Nikki'];
+document.addEventListener("DOMContentLoaded", function () {
+    let addButton = document.createElement('button');
+    // add HTML to button
+    addButton.innerHTML = "Sing";
+    // attach classname to button (used to keep button on it's own line)
+    addButton.className = "button"
+    // add button to the body
+    document.body.appendChild(addButton);
 
-for (i = 0; i < friends.length; ++i) {
-    name = friends[i]
-    console.log(name.toUpperCase());
-        for (l = 99; l > 0; l = line) {
-            line = l - 1;
-            console.log(l + ' lines of code in the file, ' + l + ' lines of code; ' + name + ' strikes one out, clears it all out, ' + line + ' lines of code in the file')
-        };
-}
+    addButton.addEventListener("click", function () {
+        let friends = ['Matt', 'Joe', 'Danica', 'MeiLee', 'Nikki'];
+
+        for (let i = 0; i < friends.length; ++i) {
+            let name = friends[i]
+            console.log(name.toUpperCase());
+
+            for (let l = 99; l > 0; l--) {
+                let line = l - 1;
+                if (l == 1) {
+                    // last line of code
+                    console.log(l + ' line of code in the file, ' + l + ' line of code; ' + name + ' strikes one out, clears it all out, no more lines of code in the file')
+                } else if (l == 2) {
+                    // second to last line of code
+                    console.log(l + ' lines of code in the file, ' + l + ' lines of code; ' + name + ' strikes one out, clears it all out, ' + line + ' line of code in the file')
+                } else {
+                    // all lines of 99-2
+                    console.log(l + ' lines of code in the file, ' + l + ' lines of code; ' + name + ' strikes one out, clears it all out, ' + line + ' lines of code in the file')
+                }
+            };
+        }
+    })
+})
